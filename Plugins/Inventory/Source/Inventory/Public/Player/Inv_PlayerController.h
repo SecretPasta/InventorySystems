19d77e9bc385aae7f62256c3c1ba00c0e80ed48c
SecretPasta <boris.shoostin@gmail.com>
2026-02-24 21:27:36 +0200

    Created primary Interact Action

--- Plugins/Inventory/Source/Inventory/Public/Player/Inv_PlayerController.h
+++ Plugins/Inventory/Source/Inventory/Public/Player/Inv_PlayerController.h
@@ -6,6 +6,9 @@
 #include "GameFramework/PlayerController.h"
 #include "Inv_PlayerController.generated.h"
 
+class UInputMappingContext;
+class UInputAction;
+
 /**
  * 
  */
@@ -17,4 +20,16 @@ class INVENTORY_API AInv_PlayerController : public APlayerController
 protected:
 	
 	virtual void BeginPlay() override;
+	
+	virtual void SetupInputComponent() override;
+
+private:
+
+	void PrimaryInteract();
+
+	UPROPERTY(EditDefaultsOnly, Category = "Inventory")
+	TObjectPtr<UInputMappingContext> DefaultIMC;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Inventory")
+	TObjectPtr<UInputAction> PrimaryInteractAction;
 };
