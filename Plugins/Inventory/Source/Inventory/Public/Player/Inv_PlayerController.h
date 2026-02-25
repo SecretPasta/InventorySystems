cde49136a673dd9d11e55d657878c97215258273
SecretPasta <boris.shoostin@gmail.com>
2026-02-25 15:09:53 +0200

    Tracing Items

--- Plugins/Inventory/Source/Inventory/Public/Player/Inv_PlayerController.h
+++ Plugins/Inventory/Source/Inventory/Public/Player/Inv_PlayerController.h
@@ -18,17 +18,22 @@ class INVENTORY_API AInv_PlayerController : public APlayerController
 {
 	GENERATED_BODY()
 	
+public:
+	AInv_PlayerController();
+	virtual void Tick(float DeltaTime) override;
+	
+	
 protected:
 	
 	virtual void BeginPlay() override;
-	
 	virtual void SetupInputComponent() override;
 
 private:
 
 	void PrimaryInteract();
-	
 	void CreateHUDWidget();
+	
+	void TraceForItem();
 
 	UPROPERTY(EditDefaultsOnly, Category = "Inventory")
 	TObjectPtr<UInputMappingContext> DefaultIMC;
@@ -41,5 +46,14 @@ private:
 	
 	UPROPERTY()
 	TObjectPtr<UInv_HUDWidget> HUDWidget;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Inventory")
+	double TraceLength;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Inventory")
+	TEnumAsByte<ECollisionChannel> ItemTraceChannel;
+
+	TWeakObjectPtr<AActor> ThisActor;
+	TWeakObjectPtr<AActor> LastActor;
 };
 
