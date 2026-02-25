40f44e39b52469a48666a305268eaf0d260501e1
SecretPasta <boris.shoostin@gmail.com>
2026-02-25 21:56:47 +0200

    Implement FastArray functions

--- Plugins/Inventory/Source/Inventory/Private/InventoryManagement/FastArray/FastArray.cpp
+++ Plugins/Inventory/Source/Inventory/Private/InventoryManagement/FastArray/FastArray.cpp
@@ -1,4 +1,6 @@
-﻿#include "InventoryManagement/FastArray/Inv_FastArray.h"
+﻿#include "InventoryManagement/Components/Inv_InventoryComponent.h"
+#include "InventoryManagement/FastArray/Inv_FastArray.h"
+#include "Items/Inv_InventoryItem.h"
 
 FInv_InventoryEntry::FInv_InventoryEntry()
 {
@@ -6,27 +8,65 @@ FInv_InventoryEntry::FInv_InventoryEntry()
 
 TArray<UInv_InventoryItem*> FInv_InventoryFastArray::GetAllItems() const
 {
-	return TArray<UInv_InventoryItem*>();
+	TArray<UInv_InventoryItem*> Results;
+	Results.Reserve(Entries.Num());
+	for (const FInv_InventoryEntry& Entry : Entries)
+	{
+		if (!IsValid(Entry.Item)) continue;
+		Results.Add(Entry.Item);
+	}
+	return Results;
 }
 
 void FInv_InventoryFastArray::PreReplicatedRemove(const TArrayView<int32> RemovedIndices, int32 FinalSize)
 {
+	UInv_InventoryComponent* IC = Cast<UInv_InventoryComponent>(OwnerComponent);
+	if (!IsValid(IC)) return;
+	for (int32 Index : RemovedIndices)
+	{
+		IC->OnItemRemoved.Broadcast(Entries[Index].Item);
+	}
 }
 
 void FInv_InventoryFastArray::PostReplicatedAdd(const TArrayView<int32> AddedIndices, int32 FinalSize)
 {
+	UInv_InventoryComponent* IC = Cast<UInv_InventoryComponent>(OwnerComponent);
+	if (!IsValid(IC)) return;
+	for (int32 Index : AddedIndices)
+	{
+		IC->OnItemAdded.Broadcast(Entries[Index].Item);
+	}
 }
 
 UInv_InventoryItem* FInv_InventoryFastArray::AddEntry(UInv_ItemComponent* ItemComponent)
 {
+	//TODO: Implement once Item Component is more complete
 	return nullptr;
 }
 
 UInv_InventoryItem* FInv_InventoryFastArray::AddEntry(UInv_InventoryItem* Item)
 {
-	return nullptr;
+	check(OwnerComponent);
+	AActor* OwningActor = OwnerComponent->GetOwner();
+	checkf(OwningActor->HasAuthority(), TEXT("AddEntry Should Only Be Called on the Server!"));
+	
+	FInv_InventoryEntry& NewEntry = Entries.AddDefaulted_GetRef();
+	NewEntry.Item = Item;
+	
+	MarkItemDirty(NewEntry);
+	return Item;
 }
 
 void FInv_InventoryFastArray::RemoveEntry(UInv_InventoryItem* Item)
 {
+	for (auto EntryIt = Entries.CreateIterator(); EntryIt; ++EntryIt)
+	{
+		FInv_InventoryEntry& Entry = *EntryIt;
+		if (Entry.Item == Item)
+		{
+			EntryIt.RemoveCurrent();
+			MarkArrayDirty();
+			return;
+		}
+	}
 }
--- Plugins/Inventory/Source/Inventory/Public/InventoryManagement/Components/Inv_InventoryComponent.h
+++ Plugins/Inventory/Source/Inventory/Public/InventoryManagement/Components/Inv_InventoryComponent.h
@@ -6,8 +6,10 @@
 #include "Components/ActorComponent.h"
 #include "Inv_InventoryComponent.generated.h"
 
-
 class UInv_InventoryBase;
+class UInv_InventoryItem;
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FInventoryItemChanged, UInv_InventoryItem*, Item);
 
 UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent), Blueprintable)
 class INVENTORY_API UInv_InventoryComponent : public UActorComponent
@@ -18,6 +20,9 @@ public:
 	UInv_InventoryComponent();
 	
 	void ToggleInventoryMenu();
+	
+	FInventoryItemChanged OnItemAdded;
+	FInventoryItemChanged OnItemRemoved;
 
 protected:
 	virtual void BeginPlay() override;
