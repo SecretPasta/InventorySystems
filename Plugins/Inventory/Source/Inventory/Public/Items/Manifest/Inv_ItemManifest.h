839b4eed4aa67c27cb0706b35ad1b7dfed4a27db
SecretPasta <boris.shoostin@gmail.com>
2026-02-26 17:32:12 +0200

    Created Item Tags

--- Plugins/Inventory/Source/Inventory/Public/Items/Manifest/Inv_ItemManifest.h
+++ Plugins/Inventory/Source/Inventory/Public/Items/Manifest/Inv_ItemManifest.h
@@ -3,6 +3,7 @@
 #include "CoreMinimal.h"
 #include "Types/Inv_GridTypes.h"
 #include "StructUtils/InstancedStruct.h"
+#include "GameplayTagContainer.h"
 
 #include "Inv_ItemManifest.generated.h"
 
@@ -19,11 +20,13 @@ struct INVENTORY_API FInv_ItemManifest
 	GENERATED_BODY()
 	
 	UInv_InventoryItem* Manifest(UObject* NewOuter);
-	
 	EInv_ItemCategory GetItemCategory() const { return ItemCategory; }
-	
+	FGameplayTag GetItemType() const { return ItemType; }
 private:
 	
 	UPROPERTY(EditAnywhere, Category = "Inventory")
 	EInv_ItemCategory ItemCategory{EInv_ItemCategory::None};
+	
+	UPROPERTY(EditAnywhere, Category = "Inventory")
+	FGameplayTag ItemType;
 };
