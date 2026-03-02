98e80318ee0008eabfb703cf9ab5a72a259aa3a9
SecretPasta <boris.shoostin@gmail.com>
2026-03-02 14:48:43 +0200

    Set up Composite Design Pattern

---
+++ Plugins/Inventory/Source/Inventory/Private/Widgets/Composite/Inv_Composite.cpp
@@ -0,0 +1,36 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Widgets/Composite/Inv_Composite.h"
+
+#include "Blueprint/WidgetTree.h"
+
+void UInv_Composite::NativeOnInitialized()
+{
+	Super::NativeOnInitialized();
+	WidgetTree->ForEachWidget([this] (UWidget* Widget)
+	{
+		
+		if (UInv_CompositeBase* Composite = Cast<UInv_CompositeBase>(Widget) ; IsValid(Composite))
+		{
+			Children.Add(Composite);
+			Composite->Collapse();
+		}
+	});
+}
+
+void UInv_Composite::ApplyFunction(FuncType Function)
+{
+	for (TObjectPtr<UInv_CompositeBase>& Child : Children)
+	{
+		Child->ApplyFunction(Function);
+	}
+}
+
+void UInv_Composite::Collapse()
+{
+	for (TObjectPtr<UInv_CompositeBase>& Child : Children)
+	{
+		Child->Collapse();
+	}
+}
