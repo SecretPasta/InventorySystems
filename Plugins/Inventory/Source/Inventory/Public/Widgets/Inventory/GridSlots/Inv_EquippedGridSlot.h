e266e6de48e9a6993db05c78e5f48743eff35b80
SecretPasta <boris.shoostin@gmail.com>
2026-03-03 18:47:16 +0200

    Created Equipped Slotted Item

--- Plugins/Inventory/Source/Inventory/Public/Widgets/Inventory/GridSlots/Inv_EquippedGridSlot.h
+++ Plugins/Inventory/Source/Inventory/Public/Widgets/Inventory/GridSlots/Inv_EquippedGridSlot.h
@@ -8,6 +8,7 @@
 #include "Inv_EquippedGridSlot.generated.h"
 
 class UImage;
+class UInv_EquippedSlottedItem;
 
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FEquippedGridSlotClicked, UInv_EquippedGridSlot*, GridSlot, const FGameplayTag&, EquipmentTypeTag);
 
@@ -25,6 +26,8 @@ public:
 	virtual void NativeOnMouseEnter(const FGeometry& InGeometry, const FPointerEvent& InMouseEvent) override;	
 	virtual void NativeOnMouseLeave(const FPointerEvent& InMouseEvent) override;
 	virtual FReply NativeOnMouseButtonDown(const FGeometry& InGeometry, const FPointerEvent& InMouseEvent) override;
+	
+	UInv_EquippedSlottedItem* OnItemEquipped(UInv_InventoryItem* Item, const FGameplayTag& EquipmentTag, float TileSize);
 	
 	FEquippedGridSlotClicked EquippedGridSlotClicked;
 	
